fd77244705c095c13fbfff079e9c7a6c1bf62117
blame <junjie09260550@163.com>
2023-11-29 15:24:42 +0800

    1/3.txt

---
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.example.demo.controller;
+
+public class TestController {
+    public static void main(String[] args) {
+
+
+        System.out.println();
+    }
+
+    public void query(int num){
+        
+    }
+}
